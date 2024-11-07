ef9db6ce81d635ac6024890730e06c8cf51f11d5
Duaa Rabie <DuaaRabie11@gmail.com>
2024-11-07 20:14:22 +0000

    update code

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,13 +7,9 @@ from typing import List
 
 
 def filter_datum(
-    fields: List[str],
-    redaction: str,
-    message: str,
-    separator: str
-) -> str:
+    fields: List[str], redaction: str, message: str,
+    separator: str) -> str:
     """ returns the log message obfuscated """
-    message = message.replace(';', separator)
     for field in fields:
         message = re.sub(rf'{field}=[^;]*', f'{field}={redaction}', message)
     return message
