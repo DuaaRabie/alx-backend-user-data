8468cd505ed3542c22a8699a5a6893e6e23caf81
Duaa Rabie <DuaaRabie11@gmail.com>
2024-11-08 00:00:37 +0000

    task3

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,11 +4,13 @@
 
 import re
 import logging
+import os
+import mysql.connector
+from mysql.connector import Error
 from typing import List
 
 
-PII_FIELDS = (
-    'name', 'email', 'phone', 'ssn', 'ip')
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'ip')
 
 
 def filter_datum(
@@ -56,3 +58,44 @@ def get_logger() -> logging.Logger:
     stream_handler.setFormatter(formatter)
     logger.addHandler(stream_handler)
     return logger
+
+
+def get_db():
+    """
+    Returns a MySQL database connection object
+    using credentials stored as environment variables.
+    The function reads the following environment variables:
+    - PERSONAL_DATA_DB_USERNAME
+    - PERSONAL_DATA_DB_PASSWORD
+    - PERSONAL_DATA_DB_HOST
+    - PERSONAL_DATA_DB_NAME
+    """
+
+    db_user = os.getenv('PERSONAL_DATA_DB_USERNAME', 'root')
+    db_password = os.getenv('PERSONAL_DATA_DB_PASSWORD', '')
+    db_host = os.getenv('PERSONAL_DATA_DB_HOST', 'localhost')
+    db_name = os.getenv('PERSONAL_DATA_DB_NAME')
+
+    # If the database name is not set, raise an error
+    name_error = "(PERSONAL_DATA_DB_NAME) is required but not set."
+    if not db_name:
+        raise ValueError(name_error)
+
+    try:
+        # Connect to the database using the retrieved credentials
+        connection = mysql.connector.connect(
+            host=db_host,
+            user=db_user,
+            password=db_password,
+            database=db_name
+        )
+
+        # Check if the connection was successful
+        if connection.is_connected():
+            return connection
+        else:
+            raise ValueError("Failed to connect to the database.")
+
+    except Error as e:
+        print(f"Error: {e}")
+        raise
