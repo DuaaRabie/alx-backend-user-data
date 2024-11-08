113d5e304a8fa77c9c19e66cdfcb660c79d12db3
Duaa Rabie <DuaaRabie11@gmail.com>
2024-11-08 01:27:07 +0000

    task5

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,16 @@
+import bcrypt
+
+def hash_password(password: str) -> bytes:
+    """
+    Hash a password using bcrypt.
+
+    Args:
+        password (str): The password to hash.
+
+    Returns:
+        bytes: The hashed password as a byte string.
+    """
+    # Generate a salt and hash the password
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password
