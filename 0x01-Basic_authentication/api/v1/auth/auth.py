1ad55e631d3bb5996d63bb23fc361530267c4f56
Duaa Rabie <DuaaRabie11@gmail.com>
2024-11-12 12:38:07 +0000

    task3

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+""" task3: Auth class to manage the API auth """
+
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """ a class to manage the API authentication """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ require auth
+        Return:
+            False - path and excluded_paths
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """ authorization header
+        Return: None - request
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ current user
+        Return: None - request"
+        return "None - request"
+        """
+        return None
