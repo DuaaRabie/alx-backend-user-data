12bbb2c79627ab8919035a2692e52f8eb12e0430
Duaa Rabie <DuaaRabie11@gmail.com>
2024-11-12 14:04:35 +0000

    task8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """ inherit from Auth class """
 
 
+import base64
 from api.v1.auth.auth import Auth
 
 
@@ -10,7 +11,7 @@ class BasicAuth(Auth):
     BasicAuth class that inherits from Auth.
     """
     def extract_base64_authorization_header(self, authorization_header: str)\
-        -> str:
+    -> str:
         """ returns the Base64 part of the Authorization header """
         if not isinstance(authorization_header, str):
             return None
@@ -22,3 +23,16 @@ class BasicAuth(Auth):
             return None
 
         return parts[1]
+
+    def decode_base64_authorization_header(self, base64_authorization_header: str)\
+    -> str:
+        """ returns the decoded value of a Base64 string """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            return decoded_bytes.decode('utf-8')
+        except (base64.binascii.Error, UnicodeDecodeError):
+            return None
