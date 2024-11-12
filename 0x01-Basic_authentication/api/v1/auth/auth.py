00368f0b82046f0c88ee9e4b11e44c2ec5c2e425
Duaa Rabie <DuaaRabie11@gmail.com>
2024-11-12 13:05:11 +0000

    task4

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -13,7 +13,18 @@ class Auth:
         Return:
             False - path and excluded_paths
         """
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+
+        if not path.endwith('/'):
+            path += '/'
+
+        if path in excluded_paths:
+            return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ authorization header
