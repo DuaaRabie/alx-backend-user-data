91e250077be7b4ff330843f6f461335e6da057eb
Duaa Rabie <DuaaRabie11@gmail.com>
2024-11-12 11:45:35 +0000

    task0

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,7 +26,7 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/api/v1/unauthorized', methods=['GET'], strict_slashes=False)
+@app_views.route('/api/v1/unauthorized', methods=['GET'])
 def unauthorized_endpoint():
     """ GET /api/v1/unauthorized
     Return:
