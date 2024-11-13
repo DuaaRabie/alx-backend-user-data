5a4a86cfc210be0fe88e57ec435f426097c220f5
Duaa Rabie <DuaaRabie11@gmail.com>
2024-11-13 23:30:23 +0000

    task3

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -9,6 +9,10 @@ class SessionAuth(Auth):
     """ Session Authentication class """
     user_id_by_session_id = {}
 
+    def __init__(self):
+        """ the constructor method """
+        super().__init__()
+
     def create_session(self, user_id: str = None) -> str:
         """ create a session method """
         if user_id is None or\
@@ -17,3 +21,11 @@ class SessionAuth(Auth):
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """ returns user ID based on session ID """
+        if session_id is None or\
+                not isinstance(session_id, str):
+            return None
+        return self.user_id_by_session_id.get(session_id)
+
