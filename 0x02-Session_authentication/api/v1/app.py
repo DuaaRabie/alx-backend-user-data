80d77ffb2dcc32de751743e992f9bb0d29cd2cf9
Duaa Rabie <DuaaRabie11@gmail.com>
2024-11-14 16:53:25 +0000

    task9

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -10,7 +10,7 @@ from flask_cors import (CORS, cross_origin)
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
 from api.v1.auth.session_auth import SessionAuth
-
+from api.v1.auth.session_exp_auth import SessionExpAuth
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -22,6 +22,8 @@ if auth_type == "basic_auth":
     auth = BasicAuth()
 elif auth_type == "session_auth":
     auth = SessionAuth()
+elif auth_type == "session_exp_auth":
+    auth = SessionExpAuth()
 else:
     auth = Auth()
 
