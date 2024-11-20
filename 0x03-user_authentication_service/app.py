ecb35311482c82a8f0fcd5480475de90a36c9623
Duaa Rabie <DuaaRabie11@gmail.com>
2024-11-20 19:57:44 +0000

    update code

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -106,7 +106,7 @@ def get_reset_password_token():
     if not email:
         abort(403)
     try:
-        reset_token = AUTH.get_reset_password_token('email')
+        reset_token = AUTH.get_reset_password_token(email)
         return jsonify({
             "email": email, "reset_token": reset_token}), 200
     except ValueError:
