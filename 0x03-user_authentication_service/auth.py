57e1cc413ba67cc1881b0fddf664c34962ec34e9
Duaa Rabie <DuaaRabie11@gmail.com>
2024-11-20 16:13:06 +0000

    update code

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -50,7 +50,7 @@ class Auth:
         """
         try:
             self._db.find_user_by(email=email)
-            raise ValueError(f'User {email} already exists')
+            raise ValueError
         except ValueError:
             raise ValueError(f'User {email} already exists')
         except Exception:
