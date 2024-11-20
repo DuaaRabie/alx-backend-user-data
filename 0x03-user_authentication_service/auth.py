50be57cb9a6c8a084a637adb6fab8026ae5e46e6
Duaa Rabie <DuaaRabie11@gmail.com>
2024-11-20 19:33:55 +0000

    task16

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -51,7 +51,7 @@ class Auth:
         """
         if not isinstance(email, str) or not isinstance(password, str):
             raise ValueError("Email and Password must be strings")
-        try: 
+        try:
             user = self._db.find_user_by(email=email)
             if user:
                 raise ValueError(f'User {email} already exists')
@@ -104,3 +104,13 @@ class Auth:
         if user:
             user.session_id = None
             self._db._session.commit()
+
+    def get_reset_password_token(self, email: str) -> str:
+        """ Generate reset password """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError
+        reset_token = _generate_uuid()
+        self._db.update_user(user.id, reset_token=reset_token)
+        return reset_token
