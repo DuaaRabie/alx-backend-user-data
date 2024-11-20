6d5d16d03d9b4a4e3672752945f33ab863f57e61
Duaa Rabie <DuaaRabie11@gmail.com>
2024-11-20 14:18:58 +0000

    update code

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -94,9 +94,8 @@ class Auth:
         except Exception:
             return None
 
-    def destroy_session(self, user_id: int) -> None:
+    def destroy_session(self, user_id: int):
         """ Destroy session
         """
         user = self._db.find_user_by(user_id=user_id)
         user.session_id = None
-        return None
