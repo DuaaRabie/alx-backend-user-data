7390254db2ec120ad12132985a3b3dd2937c828a
Duaa Rabie <DuaaRabie11@gmail.com>
2024-11-21 16:21:45 +0000

    update code

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -6,8 +6,6 @@ import bcrypt
 import uuid
 from db import DB
 from db import User
-from sqlalchemy.orm.exc import NoResultFound
-from sqlalchemy.exc import InvalidRequestError
 
 
 def _hash_password(password: str) -> bytes:
